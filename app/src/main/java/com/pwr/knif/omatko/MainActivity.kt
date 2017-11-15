8b256698486f338d0cf75000b58757e59927e244
Kamil <kamil.uhc@op.pl>
2017-11-15 10:47:30 +0100

    Change nav ids.

--- app/src/main/java/com/pwr/knif/omatko/MainActivity.kt
+++ app/src/main/java/com/pwr/knif/omatko/MainActivity.kt
@@ -58,22 +58,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_schedule -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_assessment -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_map -> {
 
             }
-            R.id.nav_manage -> {
+            R.id.nav_history -> {
 
             }
-            R.id.nav_share -> {
+            R.id.nav_sponsors -> {
 
             }
-            R.id.nav_send -> {
+            R.id.nav_contact -> {
 
             }
         }
